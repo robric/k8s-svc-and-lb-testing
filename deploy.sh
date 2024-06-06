f6d647c8e365893bf97b7e361a199510898b0c01
roberts richard <rroberts@juniper.net>
2024-06-06 17:05:18 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,20 +26,18 @@ done
 # Install K3s on the first node
 
 echo "Installing K3s on the first node..."
-multipass exec vm1 -- bash -c 'curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --cluster-init --disable servicelb" sh -'
+multipass exec vm1 -- bash -c 'curl -sfL https://get.k3s.io | K3S_TOKEN="36d331cf-91d7-4489-b558-cd0186f8167f" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --cluster-init --disable servicelb" sh -'
 
 # Wait for K3s server to be ready
 
 echo "Waiting for K3s server to be ready..."
 sleep 30
 
-TOKEN=$(multipass exec vm1 sudo cat /var/lib/rancher/k3s/server/node-token)
-
 # Join other nodes to the cluster
 
 for i in $(seq 2 $NUM_NODES); do
   echo "Joining vm$i to the cluster..."
-  multipass exec vm$i -- bash -c "curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=\"644\" K3S_URL=https://$FIRST_NODE_IP:6443 K3S_TOKEN=$TOKEN sh -"
+  multipass exec vm$i -- bash -c "curl -sfL https://get.k3s.io | K3S_TOKEN="36d331cf-91d7-4489-b558-cd0186f8167f" K3S_KUBECONFIG_MODE="644" K3S_URL=https://$FIRST_NODE_IP:6443 sh -"
 done
 
 echo "K3s cluster deployment completed."
