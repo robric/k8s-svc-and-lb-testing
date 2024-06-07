5076a4f20508222972a2e665b7b13666e00eaee1
roberts richard <rroberts@juniper.net>
2024-06-07 15:58:02 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ done
 # Install K3s on the first node
 
 echo "Installing K3s on the first node..."
-multipass exec vm1 -- bash -c 'curl -sfL https://get.k3s.io | K3S_TOKEN="36d331cf-91d7-4489-b558-cd0186f8167f" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --cluster-init --disable servicelb" sh -'
+multipass exec vm1 -- bash -c 'curl -sfL https://get.k3s.io | K3S_TOKEN="12345678" K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server --disable servicelb" sh -'
 
 # Wait for K3s server to be ready
 
@@ -37,7 +37,7 @@ sleep 30
 
 for i in $(seq 2 $NUM_NODES); do
   echo "Joining vm$i to the cluster..."
-  multipass exec vm$i -- bash -c "curl -sfL https://get.k3s.io | K3S_TOKEN="36d331cf-91d7-4489-b558-cd0186f8167f" K3S_KUBECONFIG_MODE="644" K3S_URL=https://$FIRST_NODE_IP:6443 sh -"
+  multipass exec vm$i -- bash -c "curl -sfL https://get.k3s.io | K3S_TOKEN="12345678" K3S_KUBECONFIG_MODE="644" K3S_URL=https://$FIRST_NODE_IP:6443 sh -"
 done
 
 echo "K3s cluster deployment completed."
